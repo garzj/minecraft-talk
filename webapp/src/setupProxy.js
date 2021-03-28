40891d5681ceea3d5ce600c4545dc85186817406
garzj <jojogarz123@gmail.com>
2021-03-28 16:30:35 +0200

    Change api paths

--- webapp/src/setupProxy.js
+++ webapp/src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (pkg.proxy) {
-    app.use(proxy('/socket', { target: pkg.proxy, ws: true }));
+    app.use(proxy('/api/*', { target: pkg.proxy, ws: true }));
+    app.use(proxy('/login', { target: pkg.proxy }));
     app.use(proxy('/logout', { target: pkg.proxy }));
   }
 };
