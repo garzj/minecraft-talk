2601019a9c932075ffab4aed0a28820cf9a6c90c
garzj <jojogarz123@gmail.com>
2021-10-04 13:34:36 +0200

    Add shared folder

--- backend/src/config/paths.ts
+++ backend/src/config/paths.ts
@@ -1,7 +1,12 @@
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-export const buildDir = join(__dirname, '../../../webapp/build/');
+export const rootDir =
+  process.env.NODE_ENV === 'production'
+    ? join(__dirname, '../../../../..')
+    : join(__dirname, '../../..');
+
+export const buildDir = join(rootDir, 'webapp/build');
 export const buildDirExists = existsSync(buildDir);
 
 export const indexFile = join(buildDir, 'index.html');
