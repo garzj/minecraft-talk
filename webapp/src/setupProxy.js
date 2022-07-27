a7523d93fae7534447799afc00859b4d463c4388
garzj <jojogarz123@gmail.com>
2022-07-27 11:10:36 +0200

    Upgrade webapp deps and fix vulnerabilities

--- webapp/src/setupProxy.js
+++ webapp/src/setupProxy.js
@@ -1,9 +1,9 @@
 const pkg = require('../package.json');
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (pkg.proxy) {
-    app.use(proxy('/api/*', { target: pkg.proxy, ws: true }));
-    app.use(proxy('/login', { target: pkg.proxy }));
+    app.use(createProxyMiddleware('/api/*', { target: pkg.proxy, ws: true }));
+    app.use(createProxyMiddleware('/login', { target: pkg.proxy }));
   }
 };
