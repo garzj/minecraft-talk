2e929cc274fc7002d9522c02d8ba1105b07466b2
garzj <johannes@garz.dev>
2024-02-23 02:40:42 +0100

    Require client build files only in prod

--- server/web/routes/webapp.ts
+++ server/web/routes/webapp.ts
@@ -10,10 +10,11 @@ export const webAppRouter = express.Router();
 
 if (buildDirExists) {
   webAppRouter.use(express.static(buildDir));
-} else {
-  console.log(
+} else if (process.env.NODE_ENV === 'production') {
+  console.error(
     `Could not find the web app inside ${buildDir}. Please make sure to build it properly!`
   );
+  process.exit(1)
 }
 
 webAppRouter.use((req, res) => {
